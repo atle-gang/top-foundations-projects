320c08d7e6cdafeed539a0602d05e4425ee9edb7
Atlegang Sethono <atlegangcode@gmail.com>
2024-02-21 14:31:37 +0200

    Refactor playRound function for readability

--- rock-paper-scissors-lizard-spock/script.js
+++ rock-paper-scissors-lizard-spock/script.js
@@ -7,80 +7,29 @@ function getComputerChoice() {
 
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors") {
-        return `You Win! ${playerSelection} crushes ${computerSelection}.`
-    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper") {
-        return `You Win! ${playerSelection} cuts ${computerSelection}.`
-    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "rock") {
-        return `You Win! ${playerSelection} covers ${computerSelection}.`
-    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "lizard") {
-        return `You Win! ${playerSelection} crushes ${computerSelection}.`
-    } else if (playerSelection.toLowerCase() === "lizard" && computerSelection === "spock") {
-        return `You Win! ${playerSelection} poisons ${computerSelection}.`
-    } else if (playerSelection.toLowerCase() === "spock" && computerSelection === "scissors") {
-        return `You Win! ${playerSelection} smashes ${computerSelection}.`
-    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "lizard") {
-        return `You Win! ${playerSelection} decapitates ${computerSelection}.`
-    } else if (playerSelection.toLowerCase() === "lizard" && computerSelection === "paper") {
-        return `You Win! ${playerSelection} eats ${computerSelection}.`
-    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "spock") {
-        return `You Win! ${playerSelection} disproves ${computerSelection}.`
-    } else if (playerSelection.toLowerCase() === "spock" && computerSelection === "rock") {
-        return `You Win! ${playerSelection} vaporises ${computerSelection}.`
-    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "rock") {
-        return `${playerSelection} loses to ${computerSelection}`
-    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "scissors") {
-        return `${playerSelection} loses to ${computerSelection}`
-    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "paper") {
-        return `${playerSelection} loses to ${computerSelection}`
-    } else if (playerSelection.toLowerCase() === "lizard" && computerSelection === "rock") {
-        return `${playerSelection} loses to ${computerSelection}`
-    } else if (playerSelection.toLowerCase() === "spock" && computerSelection === "lizard") {
-        return `${playerSelection} loses to ${computerSelection}`
-    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "spock") {
-        return `${playerSelection} loses to ${computerSelection}`
-    } else if (playerSelection.toLowerCase() === "lizard" && computerSelection === "scissors") {
-        return `${playerSelection} loses to ${computerSelection}`
-    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "lizard") {
-        return `${playerSelection} loses to ${computerSelection}`
-    } else if (playerSelection.toLowerCase() === "spock" && computerSelection === "paper") {
-        return `${playerSelection} loses to ${computerSelection}`
-    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "spock") {
-        return `${playerSelection} loses to ${computerSelection}`
+    const winConditions = {
+        "rock": ["scissors", "lizard"],
+        "scissors": ["paper", "lizard"],
+        "paper": ["rock", "spock"],
+        "lizard": ["spock", "paper"],
+        "spock": ["scissors", "rock"]
+    };
+
+    const playerLowerCase = playerSelection.toLowerCase();
+    const computerLowerCase = computerSelection.toLowerCase();
+    
+    if (playerLowerCase === computerLowerCase) {
+        return `Tie!`;
+    // The '?' chaining operator ensures that if key doesn't exist in the 'winConditions',
+    // it won't throw an error
+    } else if (winConditions[playerLowerCase]?.includes(computerLowerCase)) {
+        return `You Win! ${playerLowerCase} beats ${computerLowerCase}`
     } else {
-        return `Tie!`
+        return `You Lose. ${computerLowerCase} beats ${playerLowerCase}`
     }
 }
-
 
 
 const playerSelection = "rock";
 const computerSelection = "rock"
 console.log(playRound(playerSelection, computerSelection))
-
-// Lose Possibilities:
-
-// Scissors lose to Rock
-// Paper loses to Scissors
-// Rock loses to Paper
-// Lizard loses to Rock
-// Spock loses to Lizard
-// Scissors lose to Spock
-// Lizard loses to Scissors
-// Paper loses to Lizard
-// Spock loses to Paper
-// Rock loses to Spock
-
-// Win Possibilities:
-
-// Rock crushes Scissors
-// Scissors cuts Paper
-// Paper covers Rock
-// Rock crushes Lizard
-// Lizard poisons Spock
-// Spock smashes Scissors
-// Scissors decapitates Lizard
-// Lizard eats Paper
-// Paper disproves Spock
-// Spock vaporizes Rock
-
