935776333b0d9e0739e7e533aae6f58b8cbf925e
Atlegang Sethono <atlegangcode@gmail.com>
2024-02-21 13:44:55 +0200

    Implement playRound function to handle player-winning scenarios only

--- rock-paper-scissors-lizard-spock/script.js
+++ rock-paper-scissors-lizard-spock/script.js
@@ -5,4 +5,48 @@ function getComputerChoice() {
     return weaponChoice;
 }
 
-console.log(getComputerChoice())
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors") {
+        return `You Win! ${playerSelection} crushes ${computerSelection}.`
+    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper") {
+        return `You Win! ${playerSelection} cuts ${computerSelection}.`
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "rock") {
+        return `You Win! ${playerSelection} covers ${computerSelection}.`
+    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "lizard") {
+        return `You Win! ${playerSelection} crushes ${computerSelection}.`
+    } else if (playerSelection.toLowerCase() === "lizard" && computerSelection === "spock") {
+        return `You Win! ${playerSelection} poisons ${computerSelection}.`
+    } else if (playerSelection.toLowerCase() === "spock" && computerSelection === "scissors") {
+        return `You Win! ${playerSelection} smashes ${computerSelection}.`
+    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "lizard") {
+        return `You Win! ${playerSelection} decapitates ${computerSelection}.`
+    } else if (playerSelection.toLowerCase() === "lizard" && computerSelection === "paper") {
+        return `You Win! ${playerSelection} eats ${computerSelection}.`
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "spock") {
+        return `You Win! ${playerSelection} disproves ${computerSelection}.`
+    } else if (playerSelection.toLowerCase() === "spock" && computerSelection === "rock") {
+        return `You Win! ${playerSelection} vaporises ${computerSelection}.`
+    }
+    else {
+        return `Tie!`
+    }
+}
+
+
+
+const playerSelection = "rock";
+const computerSelection = "rock"
+console.log(playRound(playerSelection, computerSelection))
+
+// Rock crushes Scissors
+// Scissors cuts Paper
+// Paper covers Rock
+// Rock crushes Lizard
+// Lizard poisons Spock
+// Spock smashes Scissors
+// Scissors decapitates Lizard
+// Lizard eats Paper
+// Paper disproves Spock
+// Spock vaporizes Rock
+
